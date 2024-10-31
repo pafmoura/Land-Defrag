8f3991c0488bd286cdfaeb3201b2c1755c1b3435
Pedro Moura <100920362+pafmoura@users.noreply.github.com>
2024-10-31 09:21:18 +0000

    Tweaks UI

--- defrag-frontend/src/app/simulator/simulatorsetup/simulatorsetup.component.ts
+++ defrag-frontend/src/app/simulator/simulatorsetup/simulatorsetup.component.ts
@@ -11,6 +11,13 @@ import * as Leaflet from 'leaflet';
   styleUrl: './simulatorsetup.component.css'
 })
 export class SimulatorsetupComponent {
+
+  constructor() {
+
+   }
+
+  mapInstance!: Leaflet.Map;
+
   progress: number = 25;
   radius: number = 45;
   circumference: number = 2 * Math.PI * this.radius;
@@ -28,7 +35,46 @@ export class SimulatorsetupComponent {
     
   };
   
+
+
+
+
   
+  ngOnInit() {
+  }
+
+
+  addPolygonToMap() {
+    const polygon = Leaflet.polygon([
+      [40.955, -8.315],
+      [40.955, -8.310],
+      [40.950, -8.310],
+      [40.950, -8.315]
+    ], {
+      color: '#FF0000',
+      fillColor: '#FFAAAA',
+      fillOpacity: 0.5
+
+    });
+
+     // Adiciona eventos de mouseover e mouseout para exibir/esconder o popup
+     polygon.on('click', (e) => {
+      polygon.bindPopup(`<b>Informações do Polígono</b><br>Área: 50m²<br>Proprietário: Pedro`).openPopup();
+    });
+    
+    polygon.on('click', () => {
+      polygon.closePopup();
+    });
+
+    polygon.addTo(this.mapInstance);
+  }
+
+
+  onMapReady(map: Leaflet.Map) {
+    this.mapInstance = map;
+    this.addPolygonToMap();
+  }
+
 
 }
 
